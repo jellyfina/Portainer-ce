073364b785f4a3d20bc9c05013d605b1f24f0fea
jellyfina <88398027+jellyfina@users.noreply.github.com>
2022-06-07 23:40:19 +0800

    Update portainer-ce-x86.sh

--- portainer-ce-x86.sh
+++ portainer-ce-x86.sh
@@ -48,9 +48,7 @@ docker stop portainer
 
 docker rm portainer
 
-docker rmi portainer/portainer
-
-docker rmi portainer/portainer-ce
+docker rmi $(docker images | grep portai | tr -s ' ' | cut -d ' ' -f 3)
 echo -e "\033[32m==================================================================\033[0m"
 read -p "是否重置portainer账户密码(首次安装直接输入 y )[y/n]" user
 case $user in
